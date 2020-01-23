a64c1f35541fa7833220eaf6bef382f5bf1926b6
Meika Lei Dane <mlddane@gmail.com>
2020-01-23 22:42:50 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Overrides Read the Doc's default
+master_doc = "index"
+
 
 # -- Project information -----------------------------------------------------
 
@@ -52,6 +55,3 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# Overrides Read the Doc's default
-master_doc = "index"
